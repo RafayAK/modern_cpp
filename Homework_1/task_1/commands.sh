9f4af82e23454aea8e5c19d95e120d142080826e
Rafay Khan <rafayak16@gmail.com>
2020-07-07 22:46:00 +0500

    add comments to shell file

--- Homework_1/task_1/commands.sh
+++ Homework_1/task_1/commands.sh
@@ -2,7 +2,7 @@
 
 # echo -n -> does not print newline at the end
 
-# 1.
+# 1. Count how many lines are there in “data.dat”.
 # wc -l -> counts number of lines; 
 #     < -> removes the file name returned at the end of wc command
 echo -n "Number of lines in \"data.dat\": " && wc -l < data/data.dat 
@@ -13,23 +13,23 @@ echo -n "Number of lines in \"data.dat\": " && wc -l < data/data.dat
 #       "wc -l" to only get the count.  
 
 
-# 2.
+# 2. Count how many lines of those contain “dolor” or “dalor”?
 # grep -w -> looks only for whole words, instead of treating the word
 #            like a regex pattern
 #      -e -> marks a pattern, any number of patterns can be searched for
 #            by daisy-chaining a bunch of -e. I find this more readable
 #            The other method of macthing multiple patterns is "dolor\|dalor"
 # wc -l   -> as before counts the number of lines piped in by grep
-grep -w -e 'dolor' -e 'dalor' data/data.dat | wc -l
+echo -n "Number of lines in \"data.dat\" that contain \"dolor\" or \"dalor\"?: " && grep -w -e 'dolor' -e 'dalor' data/data.dat | wc -l
 
 
-# 3.
+# 3. Count how many words are there in “data.dat”?
 # wc -w -> counts the number of words (words are whitespace separated)
 #     < -> removes the filename at the ed giving just the count
-wc -w < data/data.dat
+echo -n "Number of words in \"data.dat\: " && wc -w < data/data.dat
 
 
-# 4.
+# 4. Count how many of those start with “mol”?
 # grep -o -> output only matches instead of entire lines where matches occur
 #      The pattern: 
 #                   - "\b" : match from word boundry, since at the start of 
@@ -39,10 +39,10 @@ wc -w < data/data.dat
 #                   - "\b" : again look for word boundry, in this case ending 
 #                            word boundry
 # wc -l : counts the lines piped in by grep 
-grep -o "\bmol\w*\b" data/data.dat | wc -l
+echo -n "Number of words in \"data.dat\ that start with \"mol\": " && grep -o "\bmol\w*\b" data/data.dat | wc -l
 
 
-# 5.
+# 5. Count how many files in “test folder” are “.txt” files?
 # ls data/test_folder/: list all dirs in the test_folder
 # grep "\.txt" : match all folder piped in by ls that end with .txt
 #      The pattern: 
@@ -52,7 +52,7 @@ grep -o "\bmol\w*\b" data/data.dat | wc -l
 #                   - "\b" : look for word boundry, in this case ending 
 #                            word boundry
 # wc -l : count all the lines piped in by grep. 
-ls data/test_folder/ | grep "\.txt\b" | wc -l
+echo -n "Number of files in \"test_foler\" with \".txt\" extenssion: " && ls data/test_folder/ | grep "\.txt\b" | wc -l
 
 
 
